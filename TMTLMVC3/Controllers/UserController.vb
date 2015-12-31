6c7089d1c82ec556da13c0c116d7a2b3d92e853b
brelcworks <brelcworks@gmail.com>
2015-12-31 23:01:17 +0530

    1ST COMMIT

---
+++ TMTLMVC3/Controllers/UserController.vb
@@ -0,0 +1,16 @@
+﻿Public Class UserController
+    Inherits System.Web.Mvc.Controller
+
+    '
+    ' GET: /User
+
+    Function Index() As ActionResult
+        Return View()
+    End Function
+    Function list() As ActionResult
+        Dim users = New List(Of USER1)
+        Dim dc As TMTLMVC = New TMTLMVC()
+        users = dc.USER1.ToList()
+        Return View(users)
+    End Function
+End Class
---
+++ TMTLMVC3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.vb" Inherits="TMTLMVC3.MvcApplication" Language="VB" %>
