636bf91cedbc9c4ffc19927c80ab3062caae916a
brelcworks <brelcworks@gmail.com>
2016-01-26 21:48:11 +0530

    RE-TEST

--- MVCINC/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCINC.MvcApplication" Language="C#" %>
---
+++ MVCINCV4.1/Controllers/MyAccountController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace MVCINCV4._1.Controllers
+{
+    public class MyAccountController : Controller
+    {
+        public ActionResult login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult login(user1 l, string ReturnUrl = "")
+        {
+            using (DB1Entities dc = new DB1Entities())
+            {
+                var user = dc.user1.Where(a => a.uid.Equals(l.uid) && a.pass.Equals(l.pass)).FirstOrDefault();
+                if (user != null)
+                {
+                    FormsAuthentication.SetAuthCookie(user.uid, false);
+                    if (Url.IsLocalUrl(ReturnUrl))
+                    {
+                        return Redirect(ReturnUrl);
+                    }
+                    else
+                    {
+                        return RedirectToAction("AfterLogin", "Home");
+                    }
+                }
+            }
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
