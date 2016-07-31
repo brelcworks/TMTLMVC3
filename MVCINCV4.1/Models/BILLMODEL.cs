3e3dd53050df8a3f6e6661c1b8f8e8cfede11f22
brelcworks <brelcworks@gmail.com>
2016-07-31 11:50:31 -0700

    BILL-INDEX-JQWINDOW

---
+++ MVCINCV4.1/Models/BILLMODEL.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCINCV4._1.Models
+{
+    public class BILLMODEL
+    {
+        public BILL BILLs { get; set; }
+        public TABLE2 STOCKs { get; set; }
+    }
+}
